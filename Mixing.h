cf002080eb88b890c88f8c084d1e20b22da73e60
Austin Baty <abaty@submit.mit.edu>
2017-09-06 21:09:00 -0400

    updates to mixing

--- Mixing.h
+++ Mixing.h
@@ -7,15 +7,18 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include "Tools.h" // needed for dR function
 
 class Mixing{
   public:
 
     inline void setTriggerName(std::string name);
+    inline void setJetCollection(std::string name);
     inline void setVzMatchingWindow(float window);   
     inline void setEvtPlaneMatchingWindow(float window);   
     inline void setMaxAttempts(int n);
- 
+    inline void setSubjetCollections(std::vector<std::string> vec); 
+
     //scans until it finds an event with matching parameters;
     void getEvent(bool reset = true);
     void getEvent(int _hiBin, bool reset = true);
@@ -24,26 +27,31 @@ class Mixing{
     int getHibin();
     int getCurrentEvtIndx();
 
+    void getBack2BackJets(std::vector< float > &pts, float etaCut, float phoPhi, float dPhiCut, float ptCut);
+    void getSubjets(std::vector< float>  &mixdR12, int nSubjetTree, float matchingCut, float etaCut, float phoPhi, float dPhiCut, float ptCut); 
 
-    Mixing(std::vector< std::string > fList);
+    Mixing(std::vector< std::string > fList, int startingIndx = 0);
     ~Mixing();
  
   private:
     std::string mixTriggerName = "HLT_HIL1MinimumBiasHF2AND_part1_v1";
-    float vzMatchingWindow = 0.5;
+    std::string jetCollection = "akCs4PFJetAnalyzer";
+    std::vector< std::string > subjetCollections;
+    float vzMatchingWindow = 5;
     float evtPlaneMatchingWindow = 0.2;
 
     unsigned int currentFileIndx = 0;
     int currentEvtIndx = -1;
     int nAttempts = 0;
     int maximumAttempts = 10000000;
+    int nSubjectTrees = 0;
    
     TFile * f;
     void openFile(int indx);
     void closeFile();
     void getNextFile();
 
-    TTree * hlt, * evt, * skim;
+    TTree * hlt, * evt, * skim, * jet, * subjet;
     void setTrees();
     void setBranches();
     
@@ -52,13 +60,27 @@ class Mixing{
     int hiBin;
     float evtPlane[29];
     float vz;
+    int nref;
+    float jetPt[1000];
+    float jetEta[1000];
+    float jetPhi[1000];
 
+    int groomnref;
+    float groomjtphi[1000];
+    float groomjteta[1000];
+    std::vector< std::vector< float >> * subjetEta = 0;
+    std::vector< std::vector< float >> * subjetPhi = 0;
+    std::vector< std::vector< float >> * subjetPt = 0;
 
     std::vector< std::string > mixFileList;
 };
 
 inline void Mixing::setTriggerName(std::string name){
   mixTriggerName = name;
+}
+
+inline void Mixing::setJetCollection(std::string name){
+  jetCollection = name;
 }
 
 inline void Mixing::setVzMatchingWindow(float window){
@@ -71,6 +93,10 @@ inline void Mixing::setEvtPlaneMatchingWindow(float window){
 
 inline void Mixing::setMaxAttempts(int n){
   maximumAttempts = n;
+}
+
+inline void Mixing::setSubjetCollections(std::vector<std::string> vec){
+  subjetCollections = vec;
 }
 
 void Mixing::openFile(int indx){ 
@@ -98,6 +124,7 @@ void Mixing::setTrees(){
   evt = (TTree*)f->Get("hiEvtAnalyzer/HiTree");
   hlt = (TTree*)f->Get("hltanalysis/HltTree");
   skim = (TTree*)f->Get("skimanalysis/HltTree");
+  jet = (TTree*)f->Get(Form("%s/t",jetCollection.c_str())); 
 }
 
 void Mixing::setBranches(){
@@ -109,6 +136,11 @@ void Mixing::setBranches(){
   skim->SetBranchAddress("phfCoincFilter3",&hfcoinc3);
   skim->SetBranchAddress("pclusterCompatibilityFilter",&pCluster);
   skim->SetBranchAddress("pprimaryVertexFilter",&pVtx);
+
+  jet->SetBranchAddress("nref",&nref);
+  jet->SetBranchAddress("jtpt",&jetPt);
+  jet->SetBranchAddress("jteta",&jetEta);
+  jet->SetBranchAddress("jtphi",&jetPhi);
 }
 
 void Mixing::getEvent(bool reset){
@@ -169,11 +201,54 @@ int Mixing::getCurrentEvtIndx(){
   return currentEvtIndx;
 }
 
-Mixing::Mixing(std::vector< std::string >  fList){
+void Mixing::getBack2BackJets(std::vector< float > &pts, float etaCut, float phoPhi, float dPhiCut, float ptCut){
+  jet->GetEntry(currentEvtIndx);
+  for(int i = 0; i<nref; i++){
+    if(jetPt[i]<ptCut) continue;
+    if(TMath::Abs(jetEta[i])>etaCut) continue;
+    if(TMath::ACos(TMath::Cos(jetPhi[i]-phoPhi)) < dPhiCut) continue;
+    pts.push_back(jetPt[i]);
+  }
+}
+
+void Mixing::getSubjets(std::vector< float>&  mixdR12, int  nSubjetTree, float matchingCut, float etaCut, float phoPhi, float dPhiCut, float ptCut){
+  jet->GetEntry(currentEvtIndx); 
+  subjet = (TTree*) f->Get(Form("%s/t",subjetCollections.at(nSubjetTree).c_str()));
+  subjet->SetBranchAddress("nref",&groomnref);
+  subjet->SetBranchAddress("jtphi",&groomjtphi);
+  subjet->SetBranchAddress("jteta",&groomjteta);
+  subjet->SetBranchAddress("jtSubJetEta",&subjetEta);
+  subjet->SetBranchAddress("jtSubJetPhi",&subjetPhi);
+  subjet->SetBranchAddress("jtSubJetPt",&subjetPt);
+  subjet->GetEntry(currentEvtIndx); 
+ 
+  for(int k = 0; k<nref; k++){
+    if(jetPt[k]<ptCut) continue;
+    if(TMath::Abs(jetEta[k])>etaCut) continue;
+    if(TMath::ACos(TMath::Cos(jetPhi[k]-phoPhi)) < dPhiCut) continue;
+    bool isMatched = false;
+
+    for(int m = 0; m<groomnref; m++){
+      if( dR(jetEta[k], jetPhi[k], groomjteta[m], groomjtphi[m]) > matchingCut) continue;
+      isMatched = true;
+      if(subjetEta->at(m).size()==2){
+        mixdR12.push_back(dR(subjetEta->at(m).at(0),subjetPhi->at(m).at(0),subjetEta->at(m).at(1),subjetPhi->at(m).at(1)));
+      }else{
+        mixdR12.push_back(-999); 
+      }
+      break;      
+    }
+    if(!isMatched){//fix by lowering pt cut on forest
+      mixdR12.push_back(-999); 
+    }
+  }
+} 
+
+Mixing::Mixing(std::vector< std::string >  fList, int startingIndx){
   std::cout << "Initializing Mixing object!" << std::endl; 
   mixFileList = fList;
   if(mixFileList.size() > 0){
-    openFile(0);
+    openFile(startingIndx);
     setTrees();
     setBranches();
   }
